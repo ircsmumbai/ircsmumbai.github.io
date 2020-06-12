945c03516757be14485440bc693d0c44f5268cb5
Ameya <ameyalaad200@gmail.com>
2020-06-12 23:13:32 +0530

    v1

---
+++ script.js
@@ -0,0 +1,8 @@
+function login() {
+	username = document.getElementById("username").value;
+	password = document.getElementById("password").value;
+	console.log(username, password);
+	window.location.href = "./diseases.html";
+}
+
+function validate() {}
